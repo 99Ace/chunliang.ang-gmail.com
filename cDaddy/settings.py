0093e2489965a930d4f0772c5091b5afaeec3513
Ubuntu <ubuntu@ip-172-31-25-93.ap-southeast-1.compute.internal>
2020-03-27 05:23:05 +0000

    Make and Migrate the user model, set up templates

--- cDaddy/settings.py
+++ cDaddy/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cDaddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,7 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     'accounts.backends.CaseInsensitiveAuth' 
 )
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
